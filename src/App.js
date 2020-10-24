f5e29100f81665dc83bf3d7b77ac7a58a1e804e6
gonovahamza <you@example.com>
2020-10-24 21:26:12 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     hamza is here 
+
+     I have updated this app and checking now my new  deployment works fine or not
+    </div>
+  );
+}
+
+export default App;
